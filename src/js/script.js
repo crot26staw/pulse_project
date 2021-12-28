0430137c4517e53667edd25a15728f206d2e7729
Dmitry <crot26staww@mail.ru>
2021-12-28 13:42:46 +0300

    adaptive carousel and catalog

--- src/js/script.js
+++ src/js/script.js
@@ -8,7 +8,8 @@ $(document).ready(function(){
             {
                 breakpoint: 992,
                 settings: {
-                    dots: true,
+                    infinite: true,
+                    dots: false,
                     arrows: false
                 }
             }
